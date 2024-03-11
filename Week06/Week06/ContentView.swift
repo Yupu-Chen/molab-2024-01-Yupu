e0e20536fc1b708ae51e74a71028c2bbe9cb7414
John Henry Thompson <jht9629@nyu.edu>
2024-03-11 12:51:44 -0400

    Update ContentView.swift

--- Week06/Week06/ContentView.swift
+++ Week06/Week06/ContentView.swift
@@ -63,166 +63,212 @@ struct ContentView: View {
                 LinearGradient(gradient: Gradient(colors: colors), startPoint: .topLeading, endPoint: .bottomTrailing)
                     .ignoresSafeArea()
                 
-                VStack{
-                    HStack{
-                        if !running && !custom{
-                            ExpandableButtonPanel(primaryItem: ExpandableButton(label: Image(systemName: "plus"), action: {
-                                tips = "questionmark.circle"
-                                countDown = 300
-                                player = players[0]
-                            }), secondaryItems: [ExpandableButton(label: Image(systemName: "bed.double"), action: {
-                                tips = "square.and.arrow.down"
-                                countDown = showPreset(key: "bed.double")?.time ?? 300
-                                player = players[showPreset(key: "bed.double")?.player ?? 0] // show preset returns the index of the player selected
-                            }), ExpandableButton(label:Image(systemName: "studentdesk"), action:{
-                                tips = "square.and.arrow.down"
-                                countDown = showPreset(key: "studentdesk")?.time ?? 300
-                                player = players[showPreset(key: "studentdesk")?.player ?? 0] // show preset returns the index of the player selected
-                            })]
-                            )
-                            .padding()
-                        }
-
-                        Spacer()
-                    }
-                    
-                    Spacer()
-                }
+                plusView
                 
-                VStack{
-                    HStack{
-                        Spacer()
-                        if !running && !custom {
-                            Button(action: {
-                                if (tips == "questionmark.circle"){
-                                    showTips.toggle()}
-                                else{
-                                    savePreset(key: "bed.double", player: players.firstIndex(of: player)!, time: countDown)
-                                }
-                            }){
-                                Image(systemName: tips)
-                                    .resizable()
-                                    .frame(width: 30, height: 30)
-                            }
-                            .padding()
-                        }
-                    }
-                    .alert(isPresented: $showTips){
-                        Alert(title: Text("How to use UltraCalm"), message: Text("1. Choose the duration of mindfulness \n2. You can tap on the timer \nto customize the time (in seconds) \n3. Choose your audio \n3. Click START to meditate")
-                              )
-                    }
-                    
+                VStack {
+                    questionMarkView
                     Spacer()
-                    if !running && !custom{
-                        HStack{
-                            Spacer(minLength: geo.size.width/20)
-                            withAnimation{
-                                Picker("", selection: $countDown) {
-                                    ForEach(durations, id:\.self) {duration in
-                                        Text("\(duration/60) Min")
-                                    }
-                                }
-                                .onChange(of: countDown) {
-                                    player?.currentTime = .zero
-                                    currentTimer = countDown
-                                }
-                            }
-                            .pickerStyle(.segmented)
-                            Spacer(minLength: geo.size.width/20)
-                        }
+                    if !running && !custom {
+                        countDownView(geo: geo)
                     }
                     // display the time
                     if !custom {
-                        Text(String(format:"%02i:%02i", currentTimer/60, currentTimer % 60))
-                            .font(.system(size: 120, weight: .bold, design: .rounded))
-                            .foregroundColor(.black)
-                            .onTapGesture {
-                                if !running {
-                                    withAnimation{
-                                        custom = true
-                                        focused = true
-                                    }
-                                }
-                            }
+                        standardTimeView
                     } else {
-                        TextField("Custom", value: $countDown, format: .number)
-                            .keyboardType(.decimalPad)
-                            .font(.system(size: 40, weight: .bold, design: .rounded))
-                            .multilineTextAlignment(.center)
-                            .foregroundColor(.black)
-                            .focused($focused)
-                            .toolbar{
-                                ToolbarItemGroup(placement: .keyboard)
-                                {
-                                    Spacer()
-                                    Button(action: {
-                                        focused = false
-                                        custom = false
-                                    }){
-                                        Text("Done")
-                                    }
-                                }
-                            }
-                            .onChange(of: countDown) {
-                                player?.currentTime = .zero
-                                currentTimer = countDown
-                            }
+                        customTimeView
                     }
-                    
                     if !custom {
-                        Button(action: {
-                            withAnimation { // make the transition smoother
-                                self.running.toggle()
-                                player?.numberOfLoops = -1
-                                if !player!.isPlaying {
-                                    print("play")
-                                    print("\(player?.currentTime ?? 10000)")
-                                    player?.play()
-                                } else {
-                                    print("paused")
-                                    player?.pause()
-                                }
-                            }
-                        }) {Text(running ? "PAUSE" : "START")
-                                .frame(width: 160, height:60)
-                                .font(.system(size:30, weight:.medium, design:.rounded))
-                                .background(Color.black.opacity(0.45))
-                                .foregroundColor(.white)
-                                .clipShape(RoundedRectangle(cornerRadius: 15.0, style: .continuous))
-                        }
-                        .buttonStyle(PlainButtonStyle())
+                        pauseStartButton
                     }
                     Spacer()
-                    
-                    HStack{
-                        if !running && !custom {
-                            Spacer(minLength: geo.size.width/20)
-                            Picker("Music", selection: $player) {
-                                ForEach (players, id:\.self){ player in
-                                    Image(systemName: icons[players.firstIndex(of: player)!])
-                                }
-                            }
-                            .pickerStyle(.segmented)
-                            .onChange(of: player) {
-                                currentTimer = countDown
-                            }
-                            Spacer(minLength: geo.size.width/20)
-                        }
-                    }
-                    .onReceive(timer, perform: { _ in
-                        if currentTimer > 0 && running
-                        {
-                            currentTimer -= 1
-                        } else if currentTimer == 0
-                        {
-                            player?.setVolume(0.0, fadeDuration: 1) //music fade out
-                        }
-                    })
+                    iconBarView(geo: geo)
                 }
             }
         }
     }
     
+    func iconBarView(geo :GeometryProxy) -> some View {
+        HStack{
+            if !running && !custom {
+                Spacer(minLength: geo.size.width/20)
+                Picker("Music", selection: $player) {
+                    ForEach (players, id:\.self){ player in
+                        Image(systemName: icons[players.firstIndex(of: player)!])
+                    }
+                }
+                .pickerStyle(.segmented)
+                .onChange(of: player) {
+                    currentTimer = countDown
+                }
+                Spacer(minLength: geo.size.width/20)
+            }
+        }
+        .onReceive(timer, perform: { _ in
+            if currentTimer > 0 && running
+            {
+                currentTimer -= 1
+            } else if currentTimer == 0
+            {
+                player?.setVolume(0.0, fadeDuration: 1) //music fade out
+            }
+        })
+    }
+    
+    var pauseStartButton : some View {
+        Button(action: {
+            withAnimation {
+                // make the transition smoother
+                self.running.toggle()
+                player?.numberOfLoops = -1
+                if !player!.isPlaying {
+                    print("play")
+                    print("\(player?.currentTime ?? 10000)")
+                    player?.play()
+                } else {
+                    print("paused")
+                    player?.pause()
+                }
+            }
+        }) {
+            Text(running ? "PAUSE" : "START")
+                .frame(width: 160, height:60)
+                .font(.system(size:30, weight:.medium, design:.rounded))
+                .background(Color.black.opacity(0.45))
+                .foregroundColor(.white)
+                .clipShape(RoundedRectangle(cornerRadius: 15.0, style: .continuous))
+        }
+        .buttonStyle(PlainButtonStyle())
+    }
+    
+    var customTimeView : some View {
+        TextField("Custom", 
+                  value: $countDown,
+                  format: .number
+        )
+        .keyboardType(.decimalPad)
+        .font(.system(size: 40, weight: .bold, design: .rounded))
+        .multilineTextAlignment(.center)
+        .foregroundColor(.black)
+        .focused($focused)
+        .toolbar{
+            ToolbarItemGroup(placement: .keyboard)
+            {
+                Spacer()
+                Button(action: {
+                    focused = false
+                    custom = false
+                }){
+                    Text("Done")
+                }
+            }
+        }
+        .onChange(of: countDown) {
+            player?.currentTime = .zero
+            currentTimer = countDown
+        }
+    }
+    
+    var standardTimeView : some View {
+        Text(String(format:"%02i:%02i",
+                    currentTimer/60,
+                    currentTimer % 60)
+        )
+        .font(.system(size: 120, weight: .bold, design: .rounded))
+        .foregroundColor(.black)
+        .onTapGesture {
+            if !running {
+                withAnimation{
+                    custom = true
+                    focused = true
+                }
+            }
+        }
+    }
+    
+    func countDownView(geo :GeometryProxy) -> some View {
+        HStack{
+            Spacer(minLength: geo.size.width/20)
+            withAnimation{
+                Picker("", selection: $countDown) {
+                    ForEach(durations, id:\.self) {duration in
+                        Text("\(duration/60) Min")
+                    }
+                }
+                .onChange(of: countDown) {
+                    player?.currentTime = .zero
+                    currentTimer = countDown
+                }
+            }
+            .pickerStyle(.segmented)
+            Spacer(minLength: geo.size.width/20)
+        }
+    }
+    
+    var questionMarkView : some View {
+        HStack{
+            Spacer()
+            if !running && !custom {
+                Button(action: {
+                    if (tips == "questionmark.circle"){
+                        showTips.toggle()}
+                    else{
+                        savePreset(key: "bed.double", player: players.firstIndex(of: player)!, time: countDown)
+                    }
+                }){
+                    Image(systemName: tips)
+                        .resizable()
+                        .frame(width: 30, height: 30)
+                }
+                .padding()
+            }
+        }
+        .alert(isPresented: $showTips){
+            Alert(title: Text("How to use UltraCalm"), message: Text("1. Choose the duration of mindfulness \n2. You can tap on the timer \nto customize the time (in seconds) \n3. Choose your audio \n3. Click START to meditate")
+            )
+        }
+    }
+    
+    var plusView : some View {
+        VStack () {
+            HStack{
+                if !running && !custom {
+                    plusViewSub
+                        .padding()
+                }
+                
+                Spacer()
+            }
+            Spacer()
+        }
+    }
+    
+    var plusViewSub : some View {
+        ExpandableButtonPanel(primaryItem: plusPrimary, secondaryItems: plusSecondary
+        )
+    }
+    
+    var plusPrimary : ExpandableButton {
+        ExpandableButton(label: Image(systemName: "plus"), action: {
+            tips = "questionmark.circle"
+            countDown = 300
+            player = players[0]
+        })
+    }
+    
+    var plusSecondary : [ExpandableButton] {
+        [
+            ExpandableButton(label: Image(systemName: "bed.double"), action: {
+                tips = "square.and.arrow.down"
+                countDown = showPreset(key: "bed.double")?.time ?? 300
+                player = players[showPreset(key: "bed.double")?.player ?? 0] // show preset returns the index of the player selected
+            }),
+            ExpandableButton(label:Image(systemName: "studentdesk"), action:{
+                tips = "square.and.arrow.down"
+                countDown = showPreset(key: "studentdesk")?.time ?? 300
+                player = players[showPreset(key: "studentdesk")?.player ?? 0] // show preset returns the index of the player selected
+            })
+        ]
+    }
     
     func savePreset(key: String, player: Int, time: Int) {
         let preset = Preset(player: player, time: time)
