db193cf3c30c612483a743a9b138fe58df9bba59
John Henry Thompson <jht9629@nyu.edu>
2024-03-24 23:46:50 -0400

    Temp fix for error: Modifying state

--- Week07 (Not Working)/Week07/ContentView.swift
+++ Week07 (Not Working)/Week07/ContentView.swift
@@ -5,8 +5,8 @@ import PhotosUI
 
 struct ContentView : View {
     
-    @State private var selectedImage: PhotosPickerItem? = nil
-    @State private var selectedImageData: Data? = nil
+    @State private var selectedImage: PhotosPickerItem?
+    @State private var selectedImageData: Data?
     
     var body: some View {
         
@@ -17,27 +17,34 @@ struct ContentView : View {
             Image(systemName: "camera")
         })
         .onChange(of: selectedImage) {
-            Task{
-                if let data = try? await
-                    selectedImage?.loadTransferable(type: Data.self){
+            Task {
+                if let data = try? await selectedImage?.loadTransferable(type: Data.self) {
                     selectedImageData = data
+                    print("selectedImage = data", data)
+                    return
                 }
+                print("selectedImage loadTransferable failed")
             }
         }
     }
 }
 
+// Temp fix for error: Modifying state during view update, this will cause undefined behavior.
+// Convert these to model class and pass into ARViewContainer
+
+var texture: TextureResource?
+var material: SimpleMaterial = SimpleMaterial(color: .white, isMetallic: true)
+var anchor: AnchorEntity = AnchorEntity(.plane(.any, classification: .any, minimumBounds: .zero))
+var entity: ModelEntity = ModelEntity(mesh: .generateBox(size: 1.0), materials: [SimpleMaterial(color: .white, isMetallic: true)])
+
 struct ARViewContainer: UIViewRepresentable {
     
     @Binding var selectedImageData: Data?
     
-    @State private var texture: TextureResource?
-    
-    @State private var material: SimpleMaterial = SimpleMaterial(color: .white, isMetallic: true)
-    
-    @State var anchor: AnchorEntity = AnchorEntity(.plane(.any, classification: .any, minimumBounds: .zero))
-    
-    @State var entity: ModelEntity = ModelEntity(mesh: .generateBox(size: 0.1), materials: [SimpleMaterial(color: .white, isMetallic: true)])
+//    @State private var texture: TextureResource?
+//    @State private var material: SimpleMaterial = SimpleMaterial(color: .white, isMetallic: true)
+//    @State var anchor: AnchorEntity = AnchorEntity(.plane(.any, classification: .any, minimumBounds: .zero))
+//    @State var entity: ModelEntity = ModelEntity(mesh: .generateBox(size: 0.5), materials: [SimpleMaterial(color: .white, isMetallic: true)])
     
     func makeUIView(context: Context) -> ARView {
         
@@ -61,8 +68,8 @@ struct ARViewContainer: UIViewRepresentable {
         config.frameSemantics.insert(.personSegmentationWithDepth)
         
         if ARWorldTrackingConfiguration.supportsSceneReconstruction(.meshWithClassification) {
-                    config.sceneReconstruction = .meshWithClassification
-                } // check if the device supports LiDR for higher precision and accuracy
+            config.sceneReconstruction = .meshWithClassification
+        } // check if the device supports LiDR for higher precision and accuracy
         
         session.run(config)
         
@@ -75,15 +82,15 @@ struct ARViewContainer: UIViewRepresentable {
         arView.addSubview(coachingOverlay)
         
         
-        self.entity = ModelEntity(mesh: .generateBox(size: 0.1), materials: [self.material])
-        self.anchor = AnchorEntity(.plane(.any, classification: .any, minimumBounds: .zero))
+        entity = ModelEntity(mesh: .generateBox(size: 0.5), materials: [material])
+        anchor = AnchorEntity(.plane(.any, classification: .any, minimumBounds: .zero))
         anchor.addChild(entity)
         arView.scene.addAnchor(anchor)
-
+        
         // Set debug options
-        #if DEBUG
+#if DEBUG
         arView.debugOptions = [.showFeaturePoints, .showWorldOrigin, .showSceneUnderstanding]
-        #endif
+#endif
         
         return arView
         
@@ -92,23 +99,33 @@ struct ARViewContainer: UIViewRepresentable {
     func updateUIView(_ uiView: ARView, context: Context) {
         let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
         
-        guard self.texture == nil else {
-                return // Texture already loaded, no need to load again
-            }
+        // disable to try to allow repeated assignement of texture
+        // yields error:
+        // Can't create new immutable asset. An asset with specified load descriptor MemoryAsset already exists.
+//        guard self.texture == nil else {
+//            print("ARViewContainer updateUIView return")
+//            return // Texture already loaded, no need to load again
+//        }
         
         if let data = self.selectedImageData {
             let filePath = documentsDirectory.appendingPathComponent("sky.png")
+            print("updateUIView data =", data)
             try? data.write(to: filePath)
             DispatchQueue.main.async {
-                self.texture = try? TextureResource.load(contentsOf: filePath)
+                texture = try? TextureResource.load(contentsOf: filePath)
+                print("updateUIView self.texture =", texture ?? "-none-")
             }
         }
         
-        if let texture = self.texture{
-            self.material.baseColor = MaterialColorParameter.texture(texture)
+        if let texture = texture {
+//            material.baseColor = MaterialColorParameter.texture(texture)
+//            self.material.color = MaterialColorParameter.texture(texture)
+            material.color = .init(tint: .white, texture: .init(texture))
+
+            print("updateUIView let texture", texture)
         }
         
-        print("updated")
+        print("updateUIView updated selectedImageData", selectedImageData ?? "-none-")
         
     }
     
@@ -123,8 +140,16 @@ class BoxAnchor: Entity, HasModel {
             materials: [self.material]
         )
     }
-
+    
     required init(from decoder: Decoder) throws {
         fatalError("init(from:) has not been implemented")
     }
 }
+
+// Is there a limit on texture image dimensions?
+// Find example of applying image texture from resource
+
+// https://stackoverflow.com/questions/72324083/applying-different-image-texture-material-to-each-side-of-the-box-in-realitykit
+
+// material.color = .init(tint: .white, texture: .init(texture))
+
